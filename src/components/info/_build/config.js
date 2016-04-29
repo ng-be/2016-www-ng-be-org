1f32a7722ffa240f86e8cbf3d6ae06e5a2d57350
Jurgen Van de Moere <jurgen.van.de.moere@gmail.com>
2016-04-29 23:44:04 +0200

    feat(info): remove component

--- src/components/info/_build/config.js
+++
@@ -1,6 +0,0 @@
-export default {
-  baseUrl: '',
-  infoDirective: {
-    name: 'ngxInfo'
-  }
-};
--- src/components/info/_build/index.js
+++
@@ -1,9 +0,0 @@
-import angular from 'angular';
-import config from './config';
-
-import infoDirective from './info.directive';
-
-export default function(ngModule, options){
-  angular.merge(config, options);
-  infoDirective(ngModule, config);
-}
