d365116b3fd10e42cda210057dd75bf66e1ee217
Jurgen Van de Moere <jurgen.van.de.moere@gmail.com>
2016-04-29 20:44:45 +0200

    feat(homepage): add component

---
+++ src/components/homepage/_build/configure-states.js
@@ -0,0 +1,29 @@
+export default function(ngModule, options){
+
+  ngModule.config(configureStates);
+
+  /**
+   * Configure states
+   *
+   * @param $stateProvider
+   */
+  function configureStates($stateProvider){
+
+    $stateProvider
+      .state('app.public.homepage', {
+        url: '/',
+        views: {
+          'content@': {
+            templateUrl: options.baseUrl + '/homepage.html',
+            controller: 'HomepageController'
+          }
+        }
+      });
+
+  }
+
+  // Inject dependencies
+  configureStates.$inject = ['$stateProvider'];
+
+}
+
